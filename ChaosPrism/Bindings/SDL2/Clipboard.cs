dbfb775add8ac9fad4440f5a646a0f6aa7d40d5f
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-07-08 12:01:29 +0300

    Add documentation to Clipboard.cs

--- ChaosPrism/Bindings/SDL2/Clipboard.cs
+++ ChaosPrism/Bindings/SDL2/Clipboard.cs
@@ -4,13 +4,26 @@ namespace ChaosPrism.Bindings;
 
 public static partial class SDL2
 {
+    /// <summary>
+    /// Puts text into the clipboard
+    /// </summary>
+    /// <param name="text">The text to store in the clipboard</param>
+    /// <returns>0 on success, or a negative error code on failure</returns>
     [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_SetClipboardText")]
     public static extern int SetClipboardText([MarshalAs(UnmanagedType.LPStr)] String text);
 
+    /// <summary>
+    /// Gets the text from clipboard
+    /// </summary>
+    /// <returns>The clipboard text on success, or an empty string on failure</returns>
     [return: MarshalAs(UnmanagedType.LPStr)]
     [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetClipboardText")]
     public static extern String GetClipboardText();
 
+    /// <summary>
+    /// Checks if the clipboard exists and contains a non-empty text string.
+    /// </summary>
+    /// <returns>True if the clipboard has text, otherwise false</returns>
     [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_HasClipboardText")]
     public static extern bool HasClipboardText();
 }
